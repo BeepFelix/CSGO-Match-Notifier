0bf2b9618511a612051369300ac33a9be7a46ba9
Jan Murken <jan_murken@hotmail.de>
2019-01-01 19:31:25 +0100

    added Websocket to notify smartphones

--- CSGO Match Notifier/Program.cs
+++ CSGO Match Notifier/Program.cs
@@ -4,12 +4,22 @@ using System.Text.RegularExpressions;
 using System.Threading;
 using System.Media;
 using System.IO;
+using System.Collections.Generic;
+
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 
 namespace CSGO_Match_Notifier
 {
 	class Program
 	{
-		private static LogReader logReader = null;
+        // Websocket
+        private static WebSocketServer wss = null;
+        private static bool useWebsocket = false;
+
+        private static LogReader logReader = null;
 		private static Regex matchAcceptRegex = new Regex(@"^Received Steam datagram ticket for server (?<serverid>\[A:\d+:\d+:\d+\]) vport \d+\. match_id=(?<matchid>\d+)$", RegexOptions.Compiled);
 		public static string CSGOInstallationPath = null;
 
@@ -37,12 +47,53 @@ namespace CSGO_Match_Notifier
 			r += "/steamapps/common/Counter-Strike Global Offensive/csgo";
 			CSGOInstallationPath = r.ToString();
 
+            // Ask for using the App
+            Console.WriteLine("Start a Websocket Server to communicate with the Match Notifier App? (y/n):");
+
+            // Check if y was pressed if yes set useWebsocket to true if not false...
+            useWebsocket = Console.Read() == 121 ? true : false;
+
 			// Start a new thread with the log reader
 			Thread logReaderThread = new Thread(() =>
 			{
 				logReader = new LogReader();
 			});
 			logReaderThread.Start();
+
+            // Only start Websocket if the user explicitly wants it...
+            if (useWebsocket)
+            {
+                // Websocket Thread for communicating with our App
+                Thread webSocketThread = new Thread(() =>
+                {
+                    Console.WriteLine("Starting Websocket...");
+                    // This IP will be used for our websocket server to bind it.
+                    string ipForBinding = "";
+
+                    // Get Local Ipv4 addresses
+                    string[] localIps = GetAllLocalIPv4(NetworkInterfaceType.Ethernet).Length != 0 ? GetAllLocalIPv4(NetworkInterfaceType.Ethernet) : GetAllLocalIPv4(NetworkInterfaceType.Wireless80211);
+
+                    // Get First Ip out ouf our localIpArray...
+                    ipForBinding = localIps.GetValue(0).ToString();
+
+                    // Create new WebSocket Server Object
+                    wss = new WebSocketServer("ws://" + ipForBinding);
+                    wss.KeepClean = false;
+                    //wss.Log.Level = LogLevel.Fatal;
+
+                    // Add new Websocket Path
+                    wss.AddWebSocketService<NotifyWhenMatchBehaviour>("/match");
+
+                    // Start Websocket
+                    wss.Start();
+
+                    if (wss.IsListening)
+                    {
+                        Console.WriteLine("Websocket Online, Connect with: " + ipForBinding + " (You need to be on the same network)");
+                    }
+                });
+                webSocketThread.Start();
+            }
 		}
 
 		public static void processLine(string line)
@@ -68,7 +119,29 @@ namespace CSGO_Match_Notifier
 				player.Play();
 			}
 
-			// and if available send a notification to our phone
-		}
-	}
+            // and if available send a notification to our phone
+            if (useWebsocket) 
+                wss.WebSocketServices.Broadcast("{\"match\": true}");
+        }
+
+        // https://stackoverflow.com/questions/6803073/get-local-ip-address
+        public static string[] GetAllLocalIPv4(NetworkInterfaceType _type)
+        {
+            List<string> ipAddrList = new List<string>();
+            foreach (NetworkInterface item in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (item.NetworkInterfaceType == _type && item.OperationalStatus == OperationalStatus.Up)
+                {
+                    foreach (UnicastIPAddressInformation ip in item.GetIPProperties().UnicastAddresses)
+                    {
+                        if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            ipAddrList.Add(ip.Address.ToString());
+                        }
+                    }
+                }
+            }
+            return ipAddrList.ToArray();
+        }
+    }
 }
