f6c8f1b5fb05e99aafe4c13187c26aa66309a48f
Jan Murken <jan_murken@hotmail.de>
2019-01-01 19:31:25 +0100

    added Websocket to notify smartphones

---
+++ CSGO Match Notifier/NotifyWhenMatchBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace CSGO_Match_Notifier
+{
+	class NotifyWhenMatchBehaviour : WebSocketBehavior
+    {
+        protected override void OnOpen()
+        {
+            Console.WriteLine("A new Connection was Opened for /match");
+        }
+    }
+}
